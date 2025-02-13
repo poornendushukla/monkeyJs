087eda32ecc56be6eae72aef70d68d5b472532e7
Poornendu Shukla <shuklapoornendu@gmail.com>
2025-02-13 23:25:19 +0530

    fix(tailwind): fixed tailwind issue

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,19 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     contentIntellisense: true,
   },
-  integrations: [
-    mdx(),
-    react(),
-    tailwind({
-      nesting: true,
-    }),
-  ],
+  vite: {
+    plugins: [tailwind()],
+  },
+  integrations: [mdx(), react()],
 });
