191533ae5f0d22a4e822177ca737fdf03bce07e4
Poornendu Shukla <38670087+poornendushukla@users.noreply.github.com>
2025-08-10 02:12:19 +0530

    fix: changes to workflow  (#15)
    * chore(fix): update
    
    * fix(tailwind): fixed tailwind issue
    
    * chore(docs): updating component
    
    * chore(docs): updating docs component
    
    * chore: docs update
    
    * chore: adding some documentation and some fi
    
    * docs: adding documentation
    
    * feat: hehe finally this would go live
    
    * chore: last push
    
    * feat: added sidebar toggle button
    
    * chore: adding docs-yaml
    
    * chore: adding umd in tsconfig.json
    
    * fix: corrected workflow

--- packages/react-monkeyjs/vite.config.ts
+++ packages/react-monkeyjs/vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [
+    react(),
+    dts({
+      include: ['lib'],
+      tsconfigPath: './tsconfig.build.json',
+    }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
@@ -13,7 +20,7 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
+      external: ['react', 'react/jsx-runtime', 'monkeyts'],
     },
   },
 });
